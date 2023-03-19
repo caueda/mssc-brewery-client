72d1c28121def22a123772eaf924ef0088ea53e8
Carlos R. Ueda <carlos.ueda@gmail.com>
2023-03-19 07:30:46 -0300

    Adding getBeerById test

--- src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -1,6 +1,8 @@
 package guru.springframework.msscbreweryclient.web.client;
 
 import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
@@ -9,9 +11,11 @@ import org.springframework.web.client.RestTemplate;
 import java.util.UUID;
 
 @Component
+@Data
+@Slf4j
 @ConfigurationProperties(value = "sfg.brewery", ignoreUnknownFields = false)
 public class BreweryClient {
-    public final String BEER_PATH_V1 = "/api/v1/beer";
+    public final String BEER_PATH_V1 = "/api/v1/beer/";
     private String apihost;
 
     private final RestTemplate restTemplate;
@@ -21,6 +25,8 @@ public class BreweryClient {
     }
 
     public BeerDto getBeerById(UUID beerId) {
-        return restTemplate.getForObject(apihost + BEER_PATH_V1 + beerId.toString(), BeerDto.class);
+        var pathUrl = apihost + BEER_PATH_V1 + beerId.toString();
+        log.info("pathUrl {}", pathUrl);
+        return restTemplate.getForObject(pathUrl, BeerDto.class);
     }
 }
